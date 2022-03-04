868b2794000a6ada33b50da3c10f4ab0831fd1cd
d068098 <pascal.wasem@sap.com>
2022-03-04 11:02:53 +0100

    initial commit

---
+++ test/developer.test.js
@@ -0,0 +1,106 @@
+/*
+▓█████▄  ▒█████   ███▄    █ ▄▄▄█████▓    ▄████▄   ██░ ██  ▄▄▄       ███▄    █   ▄████ ▓█████    ▄▄▄█████▓ ██░ ██  ██▓  ██████      █████▒██▓ ██▓    ▓█████
+▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒   ▒██▀ ▀█  ▓██░ ██▒▒████▄     ██ ▀█   █  ██▒ ▀█▒▓█   ▀    ▓  ██▒ ▓▒▓██░ ██▒▓██▒▒██    ▒    ▓██   ▒▓██▒▓██▒    ▓█   ▀
+░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒ ▓██░ ▒░   ▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░▒███      ▒ ▓██░ ▒░▒██▀▀██░▒██▒░ ▓██▄      ▒████ ░▒██▒▒██░    ▒███
+░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░ ▓██▓ ░    ▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█  ██▓▒▓█  ▄    ░ ▓██▓ ░ ░▓█ ░██ ░██░  ▒   ██▒   ░▓█▒  ░░██░▒██░    ▒▓█  ▄
+░▒████▓ ░ ████▓▒░▒██░   ▓██░  ▒██▒ ░    ▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░░▒▓███▀▒░▒████▒     ▒██▒ ░ ░▓█▒░██▓░██░▒██████▒▒   ░▒█░   ░██░░██████▒░▒████▒
+ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒   ▒ ░░      ░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ░▒   ▒ ░░ ▒░ ░     ▒ ░░    ▒ ░░▒░▒░▓  ▒ ▒▓▒ ▒ ░    ▒ ░   ░▓  ░ ▒░▓  ░░░ ▒░ ░
+ ░ ▒  ▒   ░ ▒ ▒░ ░ ░░   ░ ▒░    ░         ░  ▒    ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░  ░ ░  ░       ░     ▒ ░▒░ ░ ▒ ░░ ░▒  ░ ░    ░      ▒ ░░ ░ ▒  ░ ░ ░  ░
+ ░ ░  ░ ░ ░ ░ ▒     ░   ░ ░   ░         ░         ░  ░░ ░  ░   ▒      ░   ░ ░ ░ ░   ░    ░        ░       ░  ░░ ░ ▒ ░░  ░  ░      ░ ░    ▒ ░  ░ ░      ░
+   ░        ░ ░           ░             ░ ░       ░  ░  ░      ░  ░         ░       ░    ░  ░             ░  ░  ░ ░        ░             ░      ░  ░   ░  ░
+ ░                                      ░
+ */
+import assert from 'assert/strict'
+
+import { Developer } from '../index.js'
+
+describe('Developer', () => {
+  describe('constructor', () => {
+    it('should create a developer', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      assert.ok(developer instanceof Developer)
+    })
+  })
+
+  describe('getName', () => {
+    it('should get the name', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      assert.equal(developer.getName(), 'Nicole')
+    })
+  })
+
+  describe('setName', () => {
+    it('should set the name', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      developer.setName('John')
+      assert.equal(developer.getName(), 'John')
+    })
+  })
+
+  describe('getLanguage', () => {
+    it('should get the language', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      assert.equal(developer.getLanguage(), 'nodejs')
+    })
+  })
+
+  describe('setLanguage', () => {
+    it('should set the language', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      developer.setLanguage('java')
+      assert.equal(developer.getLanguage(), 'java')
+    })
+  })
+
+  describe('code', () => {
+    it('should resolve with node code', async () => {
+      const developer = new Developer({
+        name: 'Nicole',
+        language: 'nodejs'
+      })
+      const code = await developer.code()
+      assert.equal(code, 'console.log("Hello, Nicole!")')
+    })
+
+    it('should resolve with jave code', async () => {
+      const developer = new Developer({
+        name: 'John',
+        language: 'java'
+      })
+      const code = await developer.code()
+      assert.equal(code, 'System.out.println("Hello, John!")')
+    })
+
+    it('should resolve with python code', async () => {
+      const developer = new Developer({
+        name: 'Pete',
+        language: 'python'
+      })
+      const code = await developer.code()
+      assert.equal(code, 'print("Hello, Pete!")')
+    })
+
+    it('should reject with an error for an unknown language', async () => {
+      const developer = new Developer({
+        name: 'Adam',
+        language: 'abap'
+      })
+      await assert.rejects(developer.code(), new Error('Unsupported language: abap'))
+    })
+  })
+})
---
+++ test/hackathon.test.js
@@ -0,0 +1,57 @@
+/*
+▓█████▄  ▒█████   ███▄    █ ▄▄▄█████▓    ▄████▄   ██░ ██  ▄▄▄       ███▄    █   ▄████ ▓█████    ▄▄▄█████▓ ██░ ██  ██▓  ██████      █████▒██▓ ██▓    ▓█████
+▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒   ▒██▀ ▀█  ▓██░ ██▒▒████▄     ██ ▀█   █  ██▒ ▀█▒▓█   ▀    ▓  ██▒ ▓▒▓██░ ██▒▓██▒▒██    ▒    ▓██   ▒▓██▒▓██▒    ▓█   ▀
+░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒ ▓██░ ▒░   ▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒▒██░▄▄▄░▒███      ▒ ▓██░ ▒░▒██▀▀██░▒██▒░ ▓██▄      ▒████ ░▒██▒▒██░    ▒███
+░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░ ▓██▓ ░    ▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█  ██▓▒▓█  ▄    ░ ▓██▓ ░ ░▓█ ░██ ░██░  ▒   ██▒   ░▓█▒  ░░██░▒██░    ▒▓█  ▄
+░▒████▓ ░ ████▓▒░▒██░   ▓██░  ▒██▒ ░    ▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░░▒▓███▀▒░▒████▒     ▒██▒ ░ ░▓█▒░██▓░██░▒██████▒▒   ░▒█░   ░██░░██████▒░▒████▒
+ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒   ▒ ░░      ░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ░▒   ▒ ░░ ▒░ ░     ▒ ░░    ▒ ░░▒░▒░▓  ▒ ▒▓▒ ▒ ░    ▒ ░   ░▓  ░ ▒░▓  ░░░ ▒░ ░
+ ░ ▒  ▒   ░ ▒ ▒░ ░ ░░   ░ ▒░    ░         ░  ▒    ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░  ░   ░  ░ ░  ░       ░     ▒ ░▒░ ░ ▒ ░░ ░▒  ░ ░    ░      ▒ ░░ ░ ▒  ░ ░ ░  ░
+ ░ ░  ░ ░ ░ ░ ▒     ░   ░ ░   ░         ░         ░  ░░ ░  ░   ▒      ░   ░ ░ ░ ░   ░    ░        ░       ░  ░░ ░ ▒ ░░  ░  ░      ░ ░    ▒ ░  ░ ░      ░
+   ░        ░ ░           ░             ░ ░       ░  ░  ░      ░  ░         ░       ░    ░  ░             ░  ░  ░ ░        ░             ░      ░  ░   ░  ░
+ ░                                      ░
+ */
+import assert from 'assert/strict'
+
+import { Developer, hackathon } from '../index.js'
+
+const developers = [
+  new Developer({
+    name: 'Nicole',
+    language: 'nodejs'
+  }),
+  new Developer({
+    name: 'John',
+    language: 'java'
+  }),
+  new Developer({
+    name: 'Pete',
+    language: 'python'
+  })
+]
+describe('Hackathon', () => {
+  it('should resolve with correct code for each language', async () => {
+    const output = await hackathon(developers)
+    assert.equal(output, [
+      'console.log("Hello, Nicole!")',
+      'System.out.println("Hello, John!")',
+      'print("Hello, Pete!")'
+    ].join('\n'))
+  })
+
+  it('should resolve with an error message for an unknown language', async () => {
+    const output = await hackathon([
+      ...developers,
+      new Developer({
+        name: 'Adam',
+        language: 'abap'
+      })
+    ])
+    assert.equal(output, [
+      'console.log("Hello, Nicole!")',
+      'System.out.println("Hello, John!")',
+      'print("Hello, Pete!")',
+      'Unsupported language: abap'
+    ].join('\n'))
+  })
+})
+
