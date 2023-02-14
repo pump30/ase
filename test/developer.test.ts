bbd3df809f34a8d1d1a8ac3d5678df86581ff5c1
Sebastian Krieghoff <sebastian.krieghoff@sap.com>
2023-02-14 11:42:52 +0100

    path adapted

--- test/developer.test.ts
+++ test/developer.test.ts
@@ -12,7 +12,7 @@
  */
 import assert from 'assert/strict'
 
-import Developer from '../lib/developer.js'
+import Developer from '../src/lib/developer.js'
 
 describe('Developer', () => {
   describe('constructor', () => {
