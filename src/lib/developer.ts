2aa2c0c582aff4b283f61a672d0ddf086fe71950
Dyland <dyland.zhang@sap.com>
2024-02-28 15:13:13 +0800

    update

--- src/lib/developer.ts
+++ src/lib/developer.ts
@@ -1,2 +1,49 @@
 // TODO: Implement your developer interface and class in this file
-export default class Developer {}
+interface DeveloperInterface {
+  name: string,
+  language: string,
+  getName(): string,
+  getLanguage(): string,
+  setName(name: string): void,
+  setLanguage(language: string): void
+  code(): Promise<string>
+}
+
+
+export default class Developer implements DeveloperInterface {
+  name: string;
+  language: string;
+  constructor(name: string, language: string) {
+    this.name = name;
+    this.language = language;
+  }
+  getName(): string {
+    return this.name;
+  }
+  getLanguage(): string {
+    return this.language;
+  }
+  setLanguage(language: string): void {
+    this.language = language;
+  }
+  setName(name: string): void {
+    this.name = name;
+  }
+  code(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      switch(this.language) {
+        case "nodejs":
+          resolve(`console.log("Hello, ${this.name}!")`);
+          break;
+        case "java":
+          resolve(`System.out.println("Hello, ${this.name}!")`);
+          break;
+        case "python":
+          resolve(`print("Hello, ${this.name}!")`);
+          break;
+        default:
+          reject(Error(`Unsupported language: ${this.language}`));
+      }
+    })
+  }
+}
--- src/lib/hackathon.ts
+++ src/lib/hackathon.ts
@@ -1,1 +1,16 @@
+import DeveloperInterface from "./developer"
+
 // TODO: Implement your hackathon in this file
+export default async function hackathon(developers:Array<DeveloperInterface>): Promise<string> {
+  const res: string[] = [];
+  for(const developer of developers) {
+    try{const greeting = await developer.code();
+      res.push(greeting);}
+      catch(error){
+        if(error instanceof Error) {
+          res.push(error.message);
+        }
+      }
+  }
+  return res.join("\n")
+}
